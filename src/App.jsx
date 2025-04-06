0fafdc688c169a3286dc99df9cfa1b059b7579e1
Nonamechick <otajonovxasan350@gmail.com>
2025-04-06 12:51:12 +0500

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import Layout from "./components/Layout";
 
 
-const Home = () => <div className="max-w-7xl mx-auto p-4">Home Page</div>;
-const About = () => <div className="max-w-7xl mx-auto p-4">About Page</div>;
-const Contact = () => <div className="max-w-7xl mx-auto p-4">Contact Page</div>;
 
 function App() {
   return (
